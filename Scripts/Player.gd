cee661e1c38ad2b0b8cd81293bc03724e08bcf05
Nicolas Agustin Perez <nicolasperez95.a@gmail.com>
2023-10-03 02:18:06 -0300

    feat: update game

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,14 +1,16 @@
 extends CharacterBody2D
 
-var move_speed: float = 100.0
-var jump_force : float = 200.0
+@export var defaultDirection : bool = true
+@export var move_speed: float = 100.0
+@export var jump_force : float = 200.0
+
 var gravity : float = 500.0
-var direction : int = 1
 var can_double_jump : float = false
 var sprite : Sprite2D
 
 func _ready():
 	sprite = get_node("Sprite2D")
+	sprite.flip_h = defaultDirection
 
 func _physics_process(delta):
 	if not is_on_floor():
@@ -20,10 +22,10 @@ func _physics_process(delta):
 	
 	if Input.is_key_pressed(KEY_LEFT):
 		velocity.x -= move_speed
-		sprite.flip_h = true
+		sprite.flip_h = !defaultDirection
 	if Input.is_key_pressed(KEY_RIGHT):
 		velocity.x += move_speed
-		sprite.flip_h = false
+		sprite.flip_h = defaultDirection
 	
 	if (is_on_floor()) and Input.is_key_pressed(KEY_SPACE):
 		velocity.y = -jump_force
