e324778d1dfe0b11d23459d6e8bbe60846ef668e
Nicolas <nicolas@Nicolass-MBP.fibertel.com.ar>
2024-06-20 17:16:51 -0300

    feat: added refactor}

--- Scenes/UI/scripts/main_menu.gd
+++ Scenes/UI/scripts/main_menu.gd
@@ -1,6 +1,6 @@
 extends Control
 
-@export var start_scene: PackedScene = load("res://Scenes/main.tscn")
+@export var start_scene: PackedScene = load("res://scenes/main.tscn")
 
 func go_to_next_scene():
 	get_tree().change_scene_to_packed(start_scene)
