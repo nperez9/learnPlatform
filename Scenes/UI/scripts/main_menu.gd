31636c919325a4c8ecda1373f0d18608d007cb6b
Nicolas <nicolasperez95.a@gmail.com>
2024-06-20 01:07:19 -0300

    feat: started refactor

--- Scenes/UI/scripts/main_menu.gd
+++ Scenes/UI/scripts/main_menu.gd
@@ -1,6 +1,6 @@
 extends Control
 
-@export var start_scene: PackedScene = load("res://Scenes/main.tscn")
+@export var start_scene: PackedScene = load("res://scenes/levels/level_1.tscn")
 
 func go_to_next_scene():
 	get_tree().change_scene_to_packed(start_scene)
