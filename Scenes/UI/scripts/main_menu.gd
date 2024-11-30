2fccd5957de6a13804527d58079ba0ba75ca5e8d
HDPEREZNA <Nicolas.Perez@harley-davidson.com>
2024-11-30 19:37:20 -0300

    feat: added stuff

--- Scenes/UI/scripts/main_menu.gd
+++ Scenes/UI/scripts/main_menu.gd
@@ -8,3 +8,19 @@ func go_to_next_scene():
 func _on_start_button_pressed():
 	## Todo: add some sounds to the button
 	$AnimationPlayer.play("dissapear")
+
+
+func _on_level_1_pressed():
+	pass # Replace with function body.
+
+
+func _on_level_2_pressed():
+	pass # Replace with function body.
+
+
+func _on_level_3_pressed():
+	pass # Replace with function body.
+
+
+func _on_level_4_pressed():
+	pass # Replace with function body.
