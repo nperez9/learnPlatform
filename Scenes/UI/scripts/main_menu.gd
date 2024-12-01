a3952223aa9d9d58526713ea4edd807d0980fc2b
Nicolas Perez A <nicolasperez95.a@gmail.com>
2024-12-01 16:50:57 -0300

    feat: added level scenes

--- Scenes/UI/scripts/main_menu.gd
+++ Scenes/UI/scripts/main_menu.gd
@@ -1,26 +1,43 @@
 extends Control
 
-@export var start_scene: PackedScene = load("res://scenes/main.tscn")
+var currentLevel: PackedScene
+var level1: PackedScene = load("res://scenes/main.tscn")
+var level2: PackedScene = load("res://scenes/levels/level2.tscn")
+var level3: PackedScene = load("res://scenes/levels/level3.tscn")
+var level4: PackedScene = load("res://scenes/levels/level4.tscn")	
 
+## Plays after Fade OUt
 func go_to_next_scene():
-	get_tree().change_scene_to_packed(start_scene)
+	get_tree().change_scene_to_packed(currentLevel)
+	
+func playFadeOut():
+	$AnimationPlayer.play("dissapear")
 
 func _on_start_button_pressed():
-	## Todo: add some sounds to the button
-	$AnimationPlayer.play("dissapear")
+	currentLevel = level1
+	playFadeOut()
+	
+func _on_levels_button_pressed():
+	$StartScreen.visible = false
+	$LevelsControl.visible = true
+
+func _on_back_pressed():
+	$LevelsControl.visible = false
+	$StartScreen.visible = true
 
 
 func _on_level_1_pressed():
-	pass # Replace with function body.
-
+	currentLevel = level1
+	playFadeOut()
 
 func _on_level_2_pressed():
-	pass # Replace with function body.
-
+	currentLevel = level2
+	playFadeOut()
 
 func _on_level_3_pressed():
-	pass # Replace with function body.
-
+	currentLevel = level3
+	playFadeOut()
 
 func _on_level_4_pressed():
-	pass # Replace with function body.
+	currentLevel = level4
+	playFadeOut()
